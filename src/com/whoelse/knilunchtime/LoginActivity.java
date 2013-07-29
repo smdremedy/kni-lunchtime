00cec7823225990e728956ce9ed79d1704da8a67
sylwester.madej <sylwester.madej@whoelse.com>
2013-07-29 17:22:59 +0200

    Added: Provided resources Note: You should start here

--- src/com/whoelse/knilunchtime/LoginActivity.java
+++ src/com/whoelse/knilunchtime/LoginActivity.java
@@ -10,6 +10,6 @@ public class LoginActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_login);
     }
 }
